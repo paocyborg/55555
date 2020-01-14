75276c52b32224ff18ace9c98828b8a6bb5740a4
paocyborg <40826056+paocyborg@users.noreply.github.com>
2020-01-14 14:12:39 +0700

    Update pi.py

--- pi.py
+++ pi.py
@@ -1628,13 +1628,13 @@ def lineBot(op):
                     nama = [contact.mid for contact in group.members]
                     k = len(nama)//100
                     for a in range(k+1):
-                        txt = u''
+                        txt = ''
                         s=0
                         b=[]
                         for i in group.members[a*100 : (a+1)*100]:
                             b.append({"S":str(s), "E" :str(s+6), "M":i.mid})
                             s += 7
-                            txt += u'@Alin \n'
+                            txt += '@Alin \n'
                         line.sendMessage(to, text=txt, contentMetadata={'MENTION': json.dumps({'MENTIONEES':b})}, contentType=0)
                         line.sendMessage(to, "ทั้งหมด {} คน".format(str(len(nama))))  
                 elif text.lower() == 'ตั้งเวลา':
